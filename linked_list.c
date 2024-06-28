0242a44837b26dc58cd3d3bed654e92f66e48670
Kevin Uriel Fajardo Curiel <kfcking324@gmail.com>
2024-06-28 13:16:31 -0700

    item priotization function added, to be refined

--- linked_list.c
+++ linked_list.c
@@ -6,9 +6,11 @@
 
 void print_linked_list(node *head){ //This function is to print all items in the linked list in the right order
     node *current_pointer = head; // This line creates a temp pointer which we use to print each element, We use temp pointer so that we do not change the head 
+    int entry_value = 1;
     while (current_pointer != NULL){ // We start with the head and work our way down to the last pointer which will be NULL
-        printf("%s\n",current_pointer->element); //This line prints the element at the current pointer
+        printf(" %d. %s\n", entry_value, current_pointer->element); //This line prints the element at the current pointer
         current_pointer = current_pointer->next_element_address; // Now we increment the current pointer into the next pointer
+        entry_value = entry_value + 1;
     }//When we assign the items in the list values we will have one of those items have a pointer to NULL.
 }
 
@@ -18,6 +20,7 @@ void make_head(node **head, const char *task){ //This function is to be used to 
     strcpy(new_node->element, task);
     *head = new_node;
 }
+
 
 void linked_list_add(node **head,const char *new_element, int index){ //We start by having three inputs, address of head , 
 //the element (task) we want to put and the index at which we want to put it
@@ -63,4 +66,36 @@ void linked_list_remove(node **head, int index){
     node *node_to_del = pointer_before_index->next_element_address;
     pointer_before_index->next_element_address = node_to_del->next_element_address;
     free(node_to_del);}
+
+}
+
+void linked_list_modify(node **head, int index1, int index2) {
+    node *prior_node = NULL, *prior_node2 = NULL;
+    node *index_1 = *head, *index_2 = *head;
+
+    for (int i = 0; index_1 != NULL && i < index1; ++i) {
+        prior_node = index_1;
+        index_1 = index_1->next_element_address;
+    }
+
+    for (int i = 0; index_2 != NULL && i < index2; ++i) {
+        prior_node2 = index_2;
+        index_2 = index_2->next_element_address;
+    }
+//the edge case is confusing, I asked gpt to explain it to me and I still dont get it
+    if (prior_node != NULL) {
+        prior_node->next_element_address = index_2;
+    } else {
+        *head = index_2;
+    }
+
+    if (prior_node2 != NULL) {
+        prior_node2->next_element_address = index_1;
+    } else {
+        *head = index_1;
+    }
+
+    node *temp = index_1->next_element_address;
+    index_1->next_element_address = index_2->next_element_address;
+    index_2->next_element_address = temp;
 }
--- main.c
+++ main.c
@@ -10,7 +10,8 @@ int prompt_user(){
     printf("1. Add a new task\n");
     printf("2. View tasks\n");
     printf("3. Remove a task\n");
-    printf("4. Exit\n");
+    printf("4. Modify Order\n");
+    printf("5. Exit\n");
     scanf("%d",&user_input);
     getchar(); 
     return(user_input);
@@ -18,26 +19,25 @@ int prompt_user(){
 }
 
 int main() {
-    node *head = NULL;
-    int index;
+    node *head = NULL;//from the beginning the head pointer is pointed to NULL
+    int index = 1;
+    int index_for_modifications = 1;
     char task[20];
 
     printf("Welcome to the to-do list app\n");
-    printf("Start by adding your first task \n What would you like your first task to be?:\n");
+    printf("Start by adding your first task \nWhat would you like your first task to be?:\n");
     fgets(task,19,stdin);
     make_head(&head,task);
     
     int input =prompt_user();
-    while (input!= 4){
+    while (input!= 5){
         switch (input){
             case (1):
             printf("Enter the new task: ");
             fgets(task,20, stdin);
             printf("\n");
-            printf("Enter the position to insert the task: ");
-            scanf("%d", &index);
-            
             linked_list_add(&head, task, index);
+            index = index + 1;
                  
             break;
 
@@ -50,15 +50,28 @@ int main() {
             printf("Enter the position of the task to remove: ");
             scanf("%d", &index);
             printf("\n");
-            linked_list_remove(&head, index);
+            linked_list_remove(&head, (index-1));
             break;
+
+            case(4):
+            print_linked_list(head);
+            printf("Select the point you would like to change: ");
+            scanf("%d", &index);
+            printf("\n");
+            printf("Select where you would like to insert your task: ");
+            scanf("%d", &index_for_modifications);
+            linked_list_modify(&head, (index-1), (index_for_modifications-1));
+            print_linked_list(head);
             
+            break;
+
             default:
             printf("Invalid choice. Please try again.\n");
             printf("\n");
             break;
 
         }
+
         input =prompt_user();
     }
 
